4aab231979487b4c04fd061142fdebfff60af0f5
DC <myemail>
2026-07-11 17:02:38 -0700

    feat: update playwright.config to include additional browser options

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,10 +51,10 @@ export default defineConfig({
     // },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
@@ -65,10 +65,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+   {
+     name: 'Google Chrome',
+     use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+   },
   ],
 
   /* Run your local dev server before starting the tests */
