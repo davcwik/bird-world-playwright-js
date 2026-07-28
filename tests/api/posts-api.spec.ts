fa0357cc8733e71e6a9ba8cf597036b1581f0b8e
DC <myemail>
2026-07-27 18:05:29 -0700

    072526_add_api_tests: refactored json, form data, no payload methods in ApiBase

--- tests/api/posts-api.spec.ts
+++ tests/api/posts-api.spec.ts
@@ -2,16 +2,16 @@ import { test, expect } from '../../utils/PageFixtures';
 
 
 
-test.describe('API - Posts', { tag: ['@platform-api', '@feature-login'] }, () => {
+test.describe('API - Posts', { tag: ['@platform-api', '@feature-posts'] }, () => {
 
   test('GET /posts returns 200 response code @priority-critical', async ({ apiBase }) => {
-    const response = await apiBase.sendHttpRequestNoPayload("GET", "/wp-json/wp/v2/posts");
+    const response = await apiBase.sendHttpRequestJson("GET", "/wp-json/wp/v2/posts");
     expect(response.statusCode).toBe(200);
   }); // end test
 
 
   test('GET /posts/{id} for valid id returns expected post data and property types @priority-critical', async ({ apiBase }) => {
-    const response = await apiBase.sendHttpRequestNoPayload("GET", "/wp-json/wp/v2/posts/1");
+    const response = await apiBase.sendHttpRequestJson("GET", "/wp-json/wp/v2/posts/1");
     expect(response.statusCode).toBe(200);
     expect(response.responseData).toHaveProperty("id");
     expect(response.responseData.id).toBe(1);
@@ -20,9 +20,9 @@ test.describe('API - Posts', { tag: ['@platform-api', '@feature-login'] }, () =>
   }); // end test
 
 
-    test('GET /posts/{id} for invalid id returns expected post data and property types @priority-high', async ({ apiBase }) => {
+  test('GET /posts/{id} for invalid id returns expected post data and property types @priority-high', async ({ apiBase }) => {
 
-    const response = await apiBase.sendHttpRequestNoPayload("GET", "/wp-json/wp/v2/posts/2");
+    const response = await apiBase.sendHttpRequestJson("GET", "/wp-json/wp/v2/posts/2");
     expect(response.statusCode).toBe(404);
 
     expect(response.responseData).toHaveProperty("code");
@@ -32,7 +32,7 @@ test.describe('API - Posts', { tag: ['@platform-api', '@feature-login'] }, () =>
     expect(response.responseData).toHaveProperty("message");
     expect(response.responseData.message).toBe("Invalid post ID.");
     expect(typeof response.responseData.message).toBe("string");
-    
+
   }); // end test
 
 
