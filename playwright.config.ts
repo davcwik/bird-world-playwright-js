c24a4f5372a59ab3929f73d0083238bd09dd7f9f
DC <myemail>
2026-08-12 17:30:59 -0700

    081126_gh_pages: added disable statement in playwright.config.ts to fix WordPress redirect landing page issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,7 +61,15 @@ export default defineConfig({
     baseURL: process.env.BASE_URL, // Base URL to use in actions like `await page.goto('')`.
     trace: 'retain-on-failure', // record throughout test and keep if test fails, else auto-delete upon test completion
     screenshot: 'only-on-failure', // only taken if test fails
-    video: 'retain-on-failure', // record throughout test and keep if test fails, else auto-delete upon test completionå
+    video: 'retain-on-failure', // record throughout test and keep if test fails, else auto-delete upon test completion
+    launchOptions: {
+      args: [
+        // Fixes issue where redirect landing pages are not rendering in Playwright automation view because of how WordPress sites implement redirect transitions
+        // This results in the take-screenshot-upon-failure not executing if the test fails (ex. Upon logging out, should be redirected to the Login Page)
+        // this code disables the WordPress transition configuration so the redirect landing page now correctly renders in the Playwright automation view
+        '--disable-features=ViewTransition,SpeculationRules',
+      ],
+    },
   },
 
   // BROWSER CONFIGS //
